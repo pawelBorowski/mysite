fc6da0a77585acfc983d747be8bcb5b902d555c1
pawelBorowski <borowskip@outlook.com>
2019-05-16 22:54:09 +0200

    Added PostListView class using ListView view

--- blog/models.py
+++ blog/models.py
@@ -2,12 +2,14 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
- # each one attribute is one field in datebase
+# each one attribute is one field in datebase
+
+
 class Post(models.Model):
-    title = models.CharField(max_length = 100)
+    title = models.CharField(max_length=100)
     content = models.TextField()
-    date_posted = models.DateTimeField(default = timezone.now)
-    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
--- users/views.py
+++ users/views.py
@@ -11,17 +11,20 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
-            messages.success(request, f"Your account has been created! You are now able to log in")
+            messages.success(request, f"Your account has been created!" +
+                             "You are now able to log in")
             return redirect("login")
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {"form": form})
 
+
 @login_required
 def profile(request):
     if request.method == "POST":
         u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        p_form = ProfileUpdateForm(request.POST, request.FILES,
+                                   instance=request.user.profile)
 
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
