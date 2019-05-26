5a9c6a8798812497d24555a12f26f029fdaab311
pawelBorowski <borowskip@outlook.com>
2019-05-26 20:05:26 +0200

    Settings update

--- django_project/settings.py
+++ django_project/settings.py
@@ -145,6 +145,7 @@ EMAIL_PORT = 587
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_ACCESS_SECRET_ACCESS_KEY = os.environ.get('AWS_ACCESS_SECRET_ACCESS_KEY')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 AWS_S3_FILE_OVERWRITE = False
@@ -152,6 +153,5 @@ AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
 
 django_heroku.settings(locals())
